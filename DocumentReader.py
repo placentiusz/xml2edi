14137a5d9cf11045622ed9c39ac425d77a084153
jacekwilczynski <jacek@jablko.pl>
2020-08-04 00:27:45 +0200

    Define Product

--- DocumentReader.py
+++ DocumentReader.py
@@ -1,7 +1,19 @@
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 from xml.dom import minidom
 
+class typTowaru(Enum):
+    Towar = 1
+    Usluga = 2
+    Opakowanie = 4
+    Komplet = 8
+
+class kalkulacjaCen(Enum):
+    Narzut = 0
+    Marza = 1
+    Zysk = 2
 
 @dataclass
 class Tradesman:
@@ -12,15 +24,41 @@ class Tradesman:
     Address: str = ''
 
 @dataclass
-class Product:
-    ProductName: str
-    ProductSymbol: str
-    ProductType: str
-    ProductUnit: str
-    ProductCurrency: str
-    ProductQuantity: float
-    ProductGrossPrice: float
-    ProductVatRate: str
+class Towar:
+    typTowaruDef: typTowaru
+    kodIdentyfikacyjny: str
+    kodTowaruDostawcy: str
+    kodPaskowy: str
+    nazwa: str
+    opis: float
+    nazwaTowaryFiskalna: float
+    symbolSWWKU: str
+    pkwiu: str
+    jednostkaMiary: str
+    symbolStawkiVat: str
+    wysokoscVat: float
+    symbolStawkiVatZakup: str
+    wysokoscVatZakup: float
+    ostatniaCenaZakNetto: float
+    cenaZakWalutowa: float
+    jednostkaMiaryWalutowaZakup: str
+    kursWalutyKalkulacjiCeny: int
+    symbolwaluty: str
+    kodOpakowania: str
+    jednostkaMiaryStanumin: str
+    stanMinimalny: float
+    sredniCzasDostawy: int
+    kodIdentyfikacyjnyProdDost: str
+    dataWaznosciDzien: datetime
+    dataWaznosciDni: int
+    jednostkaMiaryObj: str
+    objetoscTwoaru: float
+    iloscGodzinwJednostceUsl: float
+    rodzajRoboczoGodziny: str
+    cenaOtwart: bool
+    uwagi: str
+    podstawaKalk: kalkulacjaCen
+    towarWazonyWagaEtyk: bool
 
 
 class ReadXMLDoc:
@@ -32,3 +70,8 @@ class ReadXMLDoc:
         name = self.document.getElementsByTagName("TradesmanName")[0]
         self.Tradesman = Tradesman('1111', '1212', name.firstChild.data, 'Somename', 'street')
         return self.Tradesman
+
+    def getProducts(self):
+        products = self.document.getElementsByTagName("Product")
+        for productxml in products:
+            print(productxml.toString())
